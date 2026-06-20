02384ff69e992fc6931b27d324e5d53349cb0d75
Lalit Mohane <lalitmohane0275.com>
2026-06-20 19:44:07 +0530

    updated url

--- frontend/src/api/todoApi.js
+++ frontend/src/api/todoApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/todos",
+  baseURL: "http://3.27.142.161:3000/api/todos",
 });
 
 export const getTodos = async () => {
